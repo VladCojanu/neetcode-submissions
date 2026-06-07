185618d17a6dd1135ad7717f7f36f0f137211c1a
Vlad Cojanu <cojanu.vlad@gmail.com>
2026-06-07 15:29:06 -0400

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,15 @@
+# from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        grouped = defaultdict(list)
+
+        for s in strs:
+            sorted_s = "".join(sorted(s))
+            grouped[sorted_s].append(s)
+        
+        return list(grouped.values())
+
+
+# space: 
+# O(n) -> n largest string
